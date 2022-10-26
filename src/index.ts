ba8442ca816545a3593aeb2b52b7c18615796e97
Dominik Müller <mulldo53@gmail.com>
2022-10-26 22:36:58 +0200

    Fix relative imgDir \w nested folders

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { LoadContext } from '@docusaurus/types';
 import { KrokiOptions, transform } from './transform';
 
-const plugin = (context: LoadContext, options: KrokiOptions) => {
-  return transform(context, options);
+const plugin = (options: KrokiOptions) => {
+  return transform(options);
 };
 
 export = plugin;
